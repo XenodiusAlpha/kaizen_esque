ca14eec79e6c1bfbffff9bccc3c4d7eb02b63d4f
sep <sep.alamouti@sepalamouti.com>
2024-01-11 20:02:53 -0400

    typeDefs-update

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,62 +1,67 @@
 const typeDefs = `
+type enrolledLesson {
 
-type Lesson {
-    _id: ID
-    title: String
-    slug: String
-    content: String
-    video: String
-    free_preview: Boolean
-    createdAt: String
-    updatedAt: String
+    slug: String!
+    completed: Boolean
+}
+
+
+  
+  type enrolledCourse {
+    slug: String!
+    lessons: [enrolledLesson]
     completed: Boolean
   }
-  
-  type Enrolled {
-    _id: ID
-    slug: String
-    lessons: [Lesson]
-    completed: Boolean
+
+  type Stripe {
+    stripe_account_id: String
+    stripe_seller: String
+    stripeSession: String
+    passwordResetCode: String
+
   }
-  
+
   type User {
-    _id: ID
-    firstName: String
-    lastName: String
-    email: String
-    password: String 
+    _id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
     picture: String
     role: [String]
     courses: [Course]
-    uenrolled: [Enrolled]
-    stripe_account_id: String
-    stripe_seller: JSON
-    stripeSession: JSON
-    createdAt: String
-    updatedAt: String
+    enrolled: [enrolledCourse]
+    stripe: Stripe
+
+    }
+
+  type Lesson {
+    _id: ID!
+    title: String!
+    slug: String!
+    content: String!
   }
   
  
   type Course {
-    _id: ID
-    name: String
-    slug: String
-    description: JSON
-    price: Float
-    image: JSON
+    _id: ID!
+    name: String!
+    slug: String!
+    description: String!
+    price: Float!
+    image: String
     category: String
     published: Boolean
     paid: Boolean
-    instructor: User
+    instructor: User!
     lessons: [Lesson]
-    createdAt: String
-    updatedAt: String
+    
   }
   
  
   type Auth {
-    token: ID
-    user: User
+    token: ID!
+    user: User!
   }
   
   type StripeAccountResponse {
@@ -70,28 +75,24 @@ type Lesson {
     user(_id: ID!): User
     courses: [Course]
     course(_id: ID!): Course
-    
+  }
   
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     updateUser(_id: ID!, firstName: String, lastName: String, email: String, password: String): User
     addCourse(name: String!, description: String!, price: Float!, category: String!, instructorId: ID!): Course
-    publishCourse(courseId: ID!): Course
-    unpublishCourse(courseId: ID!): Course
-    addLesson(courseId: ID!, title: String!, content: String!, video: String, free_preview: Boolean): Course
-    updateLesson(courseId: ID!, lessonId: ID!, title: String!, content: String!, video: String, free_preview: Boolean): Course
+    publishStatus(courseId: ID!): Course
+    addLesson(courseId: ID!, title: String!, content: String!): Course
+    updateLesson(courseId: ID!, lessonId: ID!, title: String!, content: String!): Course
     deleteLesson(courseId: ID!, lessonId: ID!): Course
-    markLessonCompleted(courseId: ID!, lessonId: ID!, userId: ID!): Completed
+    markLessonCompleted (userId:ID!, courseId: ID!, lessonId: ID!): User
     createStripeAccount: StripeAccountResponse
     handleStripeCallback: User
-    removeImage(imageUrl: String!): ImageResponse
-    confirmUpload(imageUrl: String!): Boolean!
-    checkEnrollment(courseId: ID!, userId: ID!): EnrollmentStatus
-    enrollInCourse(courseId: ID!): EnrollmentResponse
+    enrollInCourse(userId: ID!, courseId: ID!): User
   }
   
-
+  
 `;
 module.exports = typeDefs;
 
