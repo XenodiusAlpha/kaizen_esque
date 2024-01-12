2ad275209cebaa1e7f471c1e8b15b0bcb13a0f9c
Aston .A Dahllof <137734906+Valleyleaf@users.noreply.github.com>
2024-01-11 21:39:54 -0400

    Added Signup page, signup css and fixed package.json not having the right dependencies

--- client/src/components/UI/Signup.jsx
+++ client/src/components/UI/Signup.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [formInput, setformInput] = useState({
-    Username: '',
+    Firstname: '',
+    Lastname: '',
+    Email: '',
     Password: '',
+    VerifyPassword: '',
   });
   
 
@@ -18,18 +21,45 @@ const Login = () => {
     e.preventDefault();
   };
 
-  const handleBlurName = () => {
-    var inputElement = document.getElementById("warningTextNameID");
-    if (formInput.Username.trim() === '') {
+  const handleBlurFirstName = () => {
+    var inputElement = document.getElementById("warningTextFirstNameID");
+    if (formInput.Firstname.trim() === '') {
       inputElement.classList.remove("hidden-element");
     } else {
       inputElement.classList.add("hidden-element");
     }
   };
 
-  const handleBlurPassword = () => {
-    var inputElement = document.getElementById("warningTextpassword");
+  const handleBlurLastName = () => {
+    var inputElement = document.getElementById("warningTextLastNameID");
     if (formInput.Password.trim() === '') {
+      inputElement.classList.remove("hidden-element");
+    } else {
+      inputElement.classList.add("hidden-element");
+    }
+  };
+
+  const handleBlurEmail = () => {
+    var inputElement = document.getElementById("warningTextEmailID");
+    if (formInput.Email.trim() === '') {
+      inputElement.classList.remove("hidden-element");
+    } else {
+      inputElement.classList.add("hidden-element");
+    }
+  };
+
+  const handleBlurPassWord = () => {
+    var inputElement = document.getElementById("warningTextPasswordID");
+    if (formInput.Password.trim() === '') {
+      inputElement.classList.remove("hidden-element");
+    } else {
+      inputElement.classList.add("hidden-element");
+    }
+  };
+
+  const handleBlurConfirmPassWord = () => {
+    var inputElement = document.getElementById("warningTextConfirmPasswordID");
+    if (formInput.Password.trim() === handleBlurPassWord) {
       inputElement.classList.remove("hidden-element");
     } else {
       inputElement.classList.add("hidden-element");
@@ -38,36 +68,84 @@ const Login = () => {
 
   return (
     <div className=' flex-container-row center-content'>
-      <div className='lbox-style flex-container-columns Outlet-Style center-content'>
+      <div className='subox-style flex-container-columns Outlet-Style center-content'>
         <div className='flex-container-columns center-content'>
         <h2 className='wt'>Sign up</h2>
         </div>
+
         <form className='flex-container-columns center-content dpb dpt'
         onSubmit={handleSubmit}>
+
           <p className='wt'>First Name:</p>
+
           <label>
             <input
               type='text'
-              name='name'
+              name='firstName'
               value={formInput.name}
               onChange={handleChange}
               placeholder="Enter your First Name"
-              onBlur={handleBlurName}
+              onBlur={handleBlurFirstName}
             />
-            <h4 id='warningTextNameID' className='wt warningTextForm hidden-element'>*Please insert a userame</h4>
+            <h4 id='warningTextFirstNameID' className='wt warningTextForm hidden-element'>*Please insert a first name</h4>
           </label>
+
+          <p className='wt'>Last Name:</p>
+
+          <label>
+            <input
+              type='text'
+              name='lastName'
+              value={formInput.password}
+              onChange={handleChange}
+              placeholder="Enter your Last Name"
+              onBlur={handleBlurLastName}
+            />
+              <h4 id='warningTextLastNameID' className='wt warningTextForm hidden-element'>*Please insert a last name</h4>
+          </label>
+
+          <p className='wt'>Email:</p>
+
+          <label>
+            <input
+              type='text'
+              name='email'
+              value={formInput.Email}
+              onChange={handleChange}
+              placeholder="Enter your Email"
+              onBlur={handleBlurEmail}
+            />
+              <h4 id='warningTextEmailID' className='wt warningTextForm hidden-element'>*Please insert a Email</h4>
+          </label>
+
           <p className='wt'>Password:</p>
+
           <label>
             <input
               type='text'
               name='password'
-              value={formInput.password}
+              value={formInput.Email}
               onChange={handleChange}
               placeholder="Enter your Password"
-              onBlur={handleBlurPassword}
+              onBlur={handleBlurPassWord}
             />
-              <h4 id='warningTextpassword' className='wt warningTextForm hidden-element'>*Please insert a password</h4>
+              <h4 id='warningTextPasswordID' className='wt warningTextForm hidden-element'>*Please insert a password</h4>
           </label>
+
+          <p className='wt'>Confirm Password:</p>
+
+          <label>
+            <input
+              type='text'
+              name='confirmpassword'
+              value={formInput.Email}
+              onChange={handleChange}
+              placeholder="Confirm Password"
+              onBlur={handleBlurConfirmPassWord}
+            />
+              <h4 id='warningTextConfirmPasswordID' className='wt warningTextForm hidden-element'>*Passwords need to match</h4>
+          </label>
+
           <button className='button-Style' type='submit'>Sign up</button>
         </form>
         <Link to="/Login">
