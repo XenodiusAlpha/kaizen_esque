42171eb35ba9a9afdca7e1f996631ad9ee359a8b
Xenodius <xenodius@hotmail.com>
2024-01-14 22:08:41 -0400

    Added error validation using try catches for all queries and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,92 +6,147 @@ const refreshUrl = `${localBaseUrl}/restart-onboarding`;
 const returnUrl = `${localBaseUrl}/instructor/dashboard`;
 const resolvers = {
   Query: {
+
+    // Find all users
     users: async () => {
-      return await User.find({}).populate('courses').populate('enrolled');
+      try {
+        return await User.find({}).populate('courses').populate('enrolled');
+      } catch (error) {
+        throw new Error(`Failed to find users. ${error.message}`);
+      }
     },
 
+    // Find a single user by id
     user: async (_, { _id }) => {
-      return await User.findById(_id).populate('courses').populate('enrolled');
+      try {
+        return await User.findById(_id).populate('courses').populate('enrolled');
+      } catch (error) {
+        throw new Error(`Failed to find user: ${error.message}`);
+      }
     },
 
+    //  Find all courses
     courses: async () => {
-      return await Course.find({}).populate('instructor');
+      try {
+        return await Course.find({}).populate('instructor');
+      } catch (error) {
+        throw new Error(`Failed to find courses. ${error.message}`);
+      }
     },
 
+    //  Find a single course by id
     course: async (_, { _id }) => {
-      return await Course.findById(_id).populate('instructor').populate('lessons');
+      try {
+        return await Course.findById(_id).populate('instructor').populate('lessons');
+      } catch (error) {
+        throw new Error(`Failed to find course: ${error.message}`);        
+      }
     },
 
+    // Find all published courses
     publishedCourses: async (_, args) => {
-      return await Course.find({ published: true }).populate('instructor').populate('lessons');
+      try {
+        return await Course.find({ published: true }).populate('instructor').populate('lessons');
+      } catch (error) {
+        throw new Error(`Failed to find published courses. ${error.message}`);        
+      }
     },
 
+    // Verifies if user is enrolled in course
     checkEnrollment: async (_, { userId, courseId }) => {
-      const user = await User.findById(userId);
-      if (!user) {
-        throw new AuthenticationError('User not found');
+      try {
+        const user = await User.findById(userId);
+        if (!user) {
+          throw new AuthenticationError('User not found');
+        }
+        const course = await Course.findById(courseId);
+        const isEnrolled = user.enrolled.some(enrolledCourse => enrolledCourse.slug === course.slug);
+        return isEnrolled; // Directly return the boolean value
+      } catch (error) {
+        throw new Error(`Failed to find if user is enrolled in course. ${error.message}`);        
       }
-      const course = await Course.findById(courseId);
-      const isEnrolled = user.enrolled.some(enrolledCourse => enrolledCourse.slug === course.slug);
-      return isEnrolled; // Directly return the boolean value
     },
 
+    // Verifies if course is completed by user
     checkCourseCompleted: async (_, { userId, courseId }) => {
-      const user = await User.findById(userId);
-      if (!user) {
-        throw new AuthenticationError('User not found');
+      try {
+        const user = await User.findById(userId);
+        if (!user) {
+          throw new AuthenticationError('User not found');
+        }
+        const course = await Course.findById(courseId);
+        const courseEnrollment = user.enrolled.find(enrolledCourse => enrolledCourse.slug === course.slug);
+        if (!courseEnrollment) {
+          return false; // User is not enrolled in the course
+        }
+        return courseEnrollment.completed;
+      } catch (error) {
+        throw new Error(`Failed to find if user completed a course. ${error.message}`);        
       }
-      const course = await Course.findById(courseId);
-      const courseEnrollment = user.enrolled.find(enrolledCourse => enrolledCourse.slug === course.slug);
-      if (!courseEnrollment) {
-        return false; // User is not enrolled in the course
-      }
-      return courseEnrollment.completed;
     },
 
+    // Verifies if lesson is completed by user, and if the course is completed due to the last lesson being completed
     checkLessonCompleted: async (_, { userId, courseId, lessonSlug }) => {
-      const user = await User.findById(userId);
-      if (!user) {
-        throw new AuthenticationError('User not found');
+      try {
+        const user = await User.findById(userId);
+        if (!user) {
+          throw new AuthenticationError('User not found');
+        }
+        const courseEnrollment = user.enrolled.find(enrolledCourse => enrolledCourse.slug === courseId);
+        if (!courseEnrollment) {
+          return false; // User is not enrolled in the course
+        }
+        const lesson = courseEnrollment.lessons.some(lesson => lesson.slug === lessonSlug);
+        if (!lesson) {
+          return false; // Lesson not found in the course enrollment
+        }
+        return lesson.completed;
+      } catch (error) {
+        throw new Error(`Failed to find if user completed a lesson. ${error.message}`);        
       }
-      const courseEnrollment = user.enrolled.find(enrolledCourse => enrolledCourse.slug === courseId);
-      if (!courseEnrollment) {
-        return false; // User is not enrolled in the course
-      }
-      const lesson = courseEnrollment.lessons.some(lesson => lesson.slug === lessonSlug);
-      if (!lesson) {
-        return false; // Lesson not found in the course enrollment
-      }
-      return lesson.completed;
     },
 
+    // Finds courses by instructor id
     findCoursesByInstructor: async (_, { instructorId }) => {
-      return await Course.find({ instructor: instructorId }).populate('instructor').populate('lessons');
+      try {
+        return await Course.find({ instructor: instructorId }).populate('instructor').populate('lessons');
+      } catch (error) {
+        throw new Error(`Failed to find courses by instructor: ${error.message}`);        
+      }
     },
   },
 
 
   Mutation: {
+    
     // Adding a user and assigning a token to them
     addUser: async (_, { firstName, lastName, email, password }) => {
-      const user = new User({ firstName, lastName, email, password });
-      const token = signToken(user);
-      await user.save();
-      return { token, user };
+      try {
+        const user = new User({ firstName, lastName, email, password });
+        const token = signToken(user);
+        await user.save();
+        return { token, user };
+      } catch (error) {
+        throw new Error(`Failed to add a user. ${error.message}`);        
+      }
     },
 
     // Login a user by email and password and assigning a token to them
     login: async (_, { email, password }) => {
-      const user = await User.findOne({ email });
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+        const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+        const token = signToken(user);
+        return { token, user };
+      } catch (error) {
+        throw new Error(`Failed to login a user. ${error.message}`);        
       }
-      const correctPw = await user.isCorrectPassword(password);
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-      const token = signToken(user);
-      return { token, user };
     },
 
     // Adding a course from an instructor
@@ -121,20 +176,28 @@ const resolvers = {
 
         return course;
       } catch (error) {
-        throw new Error(`Failed to add course: ${error.message}`);
+        throw new Error(`Failed to add course. ${error.message}`);
       }
     },
 
     // Publish a course so that users can enroll in it
     publishCourse: async (_, { courseId }) => {
-      const course = await Course.findByIdAndUpdate(courseId, { published: true }, { new: true });
-      return course;
+      try {
+        const course = await Course.findByIdAndUpdate(courseId, { published: true }, { new: true });
+        return course;
+      } catch (error) {
+        throw new Error(`Failed to publish a course. ${error.message}`);
+      }
     },
 
     // Un-publish a course
     unpublishCourse: async (_, { courseId }) => {
-      const course = await Course.findByIdAndUpdate(courseId, { published: false }, { new: true });
-      return course;
+      try {
+        const course = await Course.findByIdAndUpdate(courseId, { published: false }, { new: true });
+        return course;
+      } catch (error) {
+        throw new Error(`Failed to unpublish a course. ${error.message}`);
+      }
     },
 
     // Enroll a user in a course
@@ -169,102 +232,122 @@ const resolvers = {
         // Return the updated user
         return user;
       } catch (error) {
-        throw new Error(`Failed to enroll in course: ${error.message}`);
+        throw new Error(`Failed to enroll in course. ${error.message}`);
       }
     },
 
+    // Add a lesson
     addLesson: async (_, { courseId, title, content }) => {
-      const course = await Course.findById(courseId);
-      if (!course) {
-        throw new Error('Course not found');
+      try {
+        const course = await Course.findById(courseId);
+        if (!course) {
+          throw new Error('Course not found');
+        }
+        const slug = title.toLowerCase().replace(/\s+/g, '-');
+        course.lessons.push({ title, slug, content });
+        await course.save();
+  
+        let users = await User.find({ 'enrolled.slug': course.slug });
+        for (let user of users) {
+          let userCourse = user.enrolled.find((enrolledCourse) => enrolledCourse.slug == course.slug);
+          userCourse.lessons = [ ...userCourse.lessons, { slug, completed: false } ];
+          await user.save();
+        }
+  
+        return course;
+      } catch (error) {
+        throw new Error(`Failed to add a lesson. ${error.message}`);
       }
-      const slug = title.toLowerCase().replace(/\s+/g, '-');
-      course.lessons.push({ title, slug, content });
-      await course.save();
-
-      let users = await User.find({ 'enrolled.slug': course.slug });
-      for (let user of users) {
-        let userCourse = user.enrolled.find((enrolledCourse) => enrolledCourse.slug == course.slug);
-        userCourse.lessons = [ ...userCourse.lessons, { slug, completed: false } ];
-        await user.save();
-      }
-
-      return course;
     },
 
+    // Update a lesson
     updateLesson: async (_, { courseId, lessonId, title, content }) => {
-      const course = await Course.findById(courseId);
-      if (!course) {
-        throw new Error('Course not found');
+      try {
+        const course = await Course.findById(courseId);
+        if (!course) {
+          throw new Error('Course not found');
+        }
+        const lesson = course.lessons.id(lessonId);
+        if (!lesson) {
+          throw new Error('Lesson not found');
+        }
+  
+        // Update all users enrolled in this course
+        if(title !== lesson.title) {
+          lesson.title = title;
+          const updatedSlug = title.toLowerCase().replace(/\s+/g, '-');
+          lesson.slug = updatedSlug;
+  
+          await User.updateMany(
+            { 'enrolled.slug': course.slug },
+            { $set: { 'enrolled.$.lessons': { slug: updatedSlug } } }
+          );
+        }
+  
+        lesson.content = content;
+        await course.save();
+        return course;
+      } catch (error) {
+        throw new Error(`Failed to update a lesson. ${error.message}`);
       }
-      const lesson = course.lessons.id(lessonId);
-      if (!lesson) {
-        throw new Error('Lesson not found');
-      }
+    },
 
-      // Update all users enrolled in this course
-      if(title !== lesson.title) {
-        lesson.title = title;
-        const updatedSlug = title.toLowerCase().replace(/\s+/g, '-');
-        lesson.slug = updatedSlug;
-
+    // Delete a lesson
+    deleteLesson: async (_, { courseId, lessonId }) => {
+      try {
+        const course = await Course.findById(courseId);
+        if (!course) {
+          throw new Error('Course not found');
+        }
+        let lessonSlug;
+        course.lessons = course.lessons.filter((lesson) => {
+          if (lesson._id != lessonId) {
+            return true;
+          }
+          else {
+            lessonSlug = lesson.slug;
+            return false;
+          }
+        });
+  
+        // Update all users enrolled in this course
         await User.updateMany(
           { 'enrolled.slug': course.slug },
-          { $set: { 'enrolled.$.lessons': { slug: updatedSlug } } }
+          { $pull: { 'enrolled.$.lessons': { slug: lessonSlug } } }
         );
+  
+        await course.save();
+        return course;
+      } catch (error) {
+        throw new Error(`Failed to delete a lesson. ${error.message}`);
       }
-
-      lesson.content = content;
-      await course.save();
-      return course;
     },
 
-    deleteLesson: async (_, { courseId, lessonId }) => {
-      const course = await Course.findById(courseId);
-      if (!course) {
-        throw new Error('Course not found');
-      }
-      let lessonSlug;
-      course.lessons = course.lessons.filter((lesson) => {
-        if (lesson._id != lessonId) {
-          return true;
-        }
-        else {
-          lessonSlug = lesson.slug;
-          return false;
-        }
-      });
-
-      // Update all users enrolled in this course
-      await User.updateMany(
-        { 'enrolled.slug': course.slug },
-        { $pull: { 'enrolled.$.lessons': { slug: lessonSlug } } }
-      );
-
-      await course.save();
-      return course;
-    },
-
+    // Logic to mark a lesson completed
     markLessonCompleted: async (_, { userId, courseSlug, lessonSlug }) => {
-      let user = await User.findById(userId);
-      if (!user) {
-        throw new Error('User not found');
+      try {
+        let user = await User.findById(userId);
+        if (!user) {
+          throw new Error('User not found');
+        }
+  
+        let enrolledCourse = user.enrolled.find((course) => course.slug == courseSlug);
+  
+        let enrolledLesson = enrolledCourse.lessons.find((lesson) => lesson.slug == lessonSlug);
+        enrolledLesson.completed = true;
+  
+        let completed = true;
+        enrolledCourse.lessons.forEach((lesson) => {
+          completed = completed && lesson.completed;
+        });
+        enrolledCourse.completed = completed;
+  
+        await user.save();
+  
+        return user;
+      } catch (error) {
+        throw new Error(`Failed to mark a lesson completed. ${error.message}`);
       }
-
-      let enrolledCourse = user.enrolled.find((course) => course.slug == courseSlug);
-
-      let enrolledLesson = enrolledCourse.lessons.find((lesson) => lesson.slug == lessonSlug);
-      enrolledLesson.completed = true;
-
-      let completed = true;
-      enrolledCourse.lessons.forEach((lesson) => {
-        completed = completed && lesson.completed;
-      });
-      enrolledCourse.completed = completed;
-
-      await user.save();
-
-      return user;
     },
 
     // Update a course in both the course model, and in the users' course and enrolled fields.
@@ -321,41 +404,46 @@ const resolvers = {
         );
         return true;
       } catch (error) {
-        console.error('Error deleting course:', error);
-        return false;
+        throw new Error('Error deleting course:', error);
       }
     },
 
+    //  Create a stripe account
     createStripeAccount: async (_, args, { user }) => {
-      if (!user) {
-        throw new AuthenticationError('Authentication required');
-      }
-      const existingUser = await User.findById(user._id);
-      if (!existingUser) {
-        throw new Error('User not found');
-      }
-      let stripeAccountId = existingUser.stripe_account_id;
-      if (!stripeAccountId) {
-        const stripeAccount = await stripe.accounts.create({
-          type: 'express',
-          email: existingUser.email,
+      try {
+        if (!user) {
+          throw new AuthenticationError('Authentication required');
+        }
+        const existingUser = await User.findById(user._id);
+        if (!existingUser) {
+          throw new Error('User not found');
+        }
+        let stripeAccountId = existingUser.stripe_account_id;
+        if (!stripeAccountId) {
+          const stripeAccount = await stripe.accounts.create({
+            type: 'express',
+            email: existingUser.email,
+          });
+          stripeAccountId = stripeAccount.id;
+          existingUser.stripe_account_id = stripeAccountId;
+          await existingUser.save();
+        }
+        const accountLink = await stripe.accountLinks.create({
+          account: stripeAccountId,
+          refresh_url: refreshUrl,
+          return_url: returnUrl,
+          type: 'account_onboarding',
         });
-        stripeAccountId = stripeAccount.id;
-        existingUser.stripe_account_id = stripeAccountId;
-        await existingUser.save();
+        return {
+          url: accountLink.url,
+          email: existingUser.email,
+        };
+      } catch (error) {
+        throw new Error('Error creating Stripe account.', error);
       }
-      const accountLink = await stripe.accountLinks.create({
-        account: stripeAccountId,
-        refresh_url: refreshUrl,
-        return_url: returnUrl,
-        type: 'account_onboarding',
-      });
-      return {
-        url: accountLink.url,
-        email: existingUser.email,
-      };
     },
 
+    // Handles Stripe Callback onboarding
     handleStripeCallback: async (_, args, { user }) => {
       if (!user) {
         throw new AuthenticationError('Authentication required');
